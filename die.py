a2f311096fac35648d1844ff60dacfeb4e04d173
lokesh-122 <94118308+lokesh-122@users.noreply.github.com>
2023-03-06 20:19:03 +0530

    Create die.py

---
+++ die.py
@@ -0,0 +1,26 @@
+import random
+n=random.randint(1,6)
+if n==1:
+  print("")
+  print("0")
+  print("")
+if n==2:
+  print("0")
+  print("")
+  print("0")
+if n==3:
+  print("0")
+  print("0")
+  print("0")
+if n==4:
+  print("0  0")
+  print(" ")
+  print("0  0")
+if n==5:
+  print("0   0")
+  print("  0")
+  print("0   0")
+ if n==6:
+  print("0 0")
+  print("0 0")
+  print("0 0")
